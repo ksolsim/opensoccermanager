a80bc36ae956c184f906b4991a022d5b394ae6f1
Andrew Steele <andrew@andrewsteele.me.uk>
2015-01-12 15:56:53 +0000

    Update shebang in all files

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 active_screen = None        # Track screen currently in view
 database = None
--- version.py
+++ version.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 NAME = "OpenSoccerManager"
 VERSION = 0.01
