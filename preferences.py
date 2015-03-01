6297cfd6b6e87c22968ab8b61d2e0598470fff3c
Andrew Steele <andrew@andrewsteele.me.uk>
2015-03-01 13:59:23 +0000

    Window size and state now saved when quitting

--- preferences.py
+++ preferences.py
@@ -11,16 +11,20 @@ class Preferences(ConfigParser):
         ConfigParser.__init__(self)
 
         home = os.path.expanduser("~")
-
         data = os.path.join(home, ".config", "opensoccermanager")
         save = os.path.join(home, ".config", "opensoccermanager", "saves")
 
+        self.filename = os.path.join(data, "preferences.ini")
+
         self["AUDIO"] = {"PlayMusic": False}
-        self["INTERFACE"] = {"Currency": 0, "StartScreen": 1}
+        self["INTERFACE"] = {"Currency": 0,
+                             "StartScreen": 1,
+                             "Maximized": False,
+                             "Width": 780,
+                             "Height": 480,
+                            }
         self["DATABASE"] = {"Database": "osm1415.db"}
         self["SAVE"] = {"Data": data, "Saves": save}
-
-        self.filename = os.path.join(data, "preferences.ini")
 
     def writefile(self):
         with open(self.filename, "w") as configfile:
@@ -31,6 +35,16 @@ class Preferences(ConfigParser):
 
         game.currency = int(self["INTERFACE"]["Currency"])
         game.start_screen = int(self["INTERFACE"]["StartScreen"])
+
+        maximized = bool(self["INTERFACE"].getboolean("Maximized"))
+
+        if maximized:
+            game.window.maximize()
+
+        width = int(self["INTERFACE"]["Width"])
+        height = int(self["INTERFACE"]["Height"])
+        game.window.set_default_size(width, height)
+
         game.music = self["AUDIO"].getboolean("PlayMusic")
         game.save_location = self["SAVE"]["Saves"]
         game.data_location = self["SAVE"]["Data"]
