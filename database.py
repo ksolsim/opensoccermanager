14d1d08edf59b03ddc0343ba7eb711dc5fc703ad
Andrew Steele <andrew@andrewsteele.me.uk>
2015-06-21 09:30:15 +0100

    Added support for apsw database bindings to be used

--- database.py
+++ database.py
@@ -16,36 +16,81 @@
 #  OpenSoccerManager.  If not, see <http://www.gnu.org/licenses/>.
 
 
-import sqlite3
 import os
 
 import game
 import preferences
 
+try:
+    import sqlite3
+except ImportError:
+    try:
+        import apsw
+    except ImportError:
+        pass
+
 
 class Database:
+    class SQLiteDatabase:
+        def __init__(self):
+            self.connection = None
+            self.cursor = None
+
+        def connect(self, filename=None):
+            if not filename:
+                preferences.preferences.readfile()
+                filepath = os.path.join("databases", game.database_filename)
+
+                if not os.path.isfile(filepath):
+                    return False
+            else:
+                filepath = filename
+
+            self.connection = sqlite3.connect(filepath)
+            self.connection.execute("PRAGMA foreign_keys = on")
+            self.cursor = self.connection.cursor()
+
+            return True
+
+        def importer(self, table):
+            self.cursor.execute("SELECT * FROM %s" % (table))
+            data = self.cursor.fetchall()
+
+            return data
+
+    class APSWDatabase:
+        def __init__(self):
+            self.connection = None
+            self.cursor = None
+
+        def connect(self, filename=None):
+            if not filename:
+                preferences.preferences.readfile()
+                filepath = os.path.join("databases", game.database_filename)
+
+                if not os.path.isfile(filepath):
+                    return False
+            else:
+                filepath = filename
+
+            self.connection = apsw.Connection(filepath)
+            self.cursor = self.connection.cursor()
+
+            return True
+
+        def importer(self, table):
+            self.cursor.execute("SELECT * FROM %s" % (table))
+            data = self.cursor.fetchall()
+
+            return data
+
     def __init__(self):
-        self.connection = None
-        self.cursor = None
+        self.database = None
 
-    def connect(self, filename=None):
-        if not filename:
-            preferences.preferences.readfile()
-            filepath = os.path.join("databases", game.database_filename)
-
-            if not os.path.isfile(filepath):
-                return False
+        if "sqlite3" in globals():
+            self.database = self.SQLiteDatabase()
         else:
-            filepath = filename
+            self.database = self.APSWDatabase()
 
-        self.connection = sqlite3.connect(filepath)
-        self.connection.execute("PRAGMA foreign_keys = on")
-        self.cursor = self.connection.cursor()
-
-        return True
-
-    def importer(self, table):
-        self.cursor.execute("SELECT * FROM %s" % (table))
-        data = self.cursor.fetchall()
-
-        return data
+    def get_database_object(self):
+        return self.database
