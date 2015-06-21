476f4c3c8754d4f02341b809c2684bb046d343bc
Andrew Steele <andrew@andrewsteele.me.uk>
2015-06-21 16:57:15 +0100

    Bump version number for 0.11

--- version.py
+++ version.py
@@ -17,8 +17,8 @@
 
 
 NAME = "OpenSoccerManager"
-VERSION = 0.10
-DATE = "20150610"
+VERSION = 0.11
+DATE = "20150618"
 COMMENTS = "Free software soccer management game written in Python and GTK+."
 WEBSITE = "http://opensoccermanager.org/"
 AUTHORS = "Andrew Steele (andrew@andrewsteele.me.uk)"
